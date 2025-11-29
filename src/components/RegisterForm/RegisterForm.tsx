6d99593b6a9862b8ab8f1cc6b4b1e1a3e7704ba9
安田海音 <yasudakaito@2.local>
2025-11-29 14:45:45 +0900

    [wip] 新規登録_実装途中

--- src/components/RegisterForm/RegisterForm.tsx
+++ src/components/RegisterForm/RegisterForm.tsx
@@ -1,7 +1,10 @@
 import { useState } from 'react';
 import { actions, formCard, input, primaryBtn, textarea } from './RegisterForm.styles';
+import { TaskType } from '../../types.ts';
+type Props = {setTaskList:React.Dispatch<React.SetStateAction<TaskType[]>>}
+//↑これ合っている？
 
-export const RegisterForm = () => {
+export const RegisterForm = ({setTaskList}:Props) => {
   const [title, setTitle] = useState('');
   const [detail, setDetail] = useState('');
 
@@ -10,14 +13,25 @@ export const RegisterForm = () => {
    */
   const onSubmitForm = (e: React.FormEvent) => {
     e.preventDefault();
-    // ここに追加ボタン押下時の処理を書く
+    //「タイトル」・「TODO」を入力後、追加ボタン押下でタスクカードを追加 taskList,setTaskListを書き換える
+    //「タイトル」・「TODO」のいずれかが空欄の場合には登録できないようにする
+    // task
+    // idは重複が許されない→数値化された新しい時間の値をidに入れるdate.now()を使用
+
+    const newTask = {
+    id: Date.now(),
+    title: title,
+    detail: detail,
+    }
+    console.log("newtask")
+    setTaskList([])
   };
 
   return (
     <form style={formCard} onSubmit={(e) => onSubmitForm(e)}>
-      <input style={input} type='text' value={title} onChange={(e) => setTitle(e.target.value)} />
+      <input style={input} type='text' value={title} onChange={(e) => setTitle(e.target.value)} placeholder='タイトルを入力a'/>
       <br />
-      <textarea style={textarea} value={detail} onChange={(e) => setDetail(e.target.value)} rows={7}></textarea>
+      <textarea style={textarea} value={detail} onChange={(e) => setDetail(e.target.value)} rows={7} placeholder='TODO を入力'></textarea>
       <div style={actions}>
         <button style={primaryBtn(true)} type='submit'>
           追加
