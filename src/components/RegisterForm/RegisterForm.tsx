83e209836778311d9db78d2613e74f08e0e040e4
安田海音 <yasudakaito@2.local>
2025-11-29 15:08:15 +0900

    [wip] 新規登録_実装途中_1510

--- src/components/RegisterForm/RegisterForm.tsx
+++ src/components/RegisterForm/RegisterForm.tsx
@@ -8,6 +8,11 @@ export const RegisterForm = ({setTaskList}:Props) => {
   const [title, setTitle] = useState('');
   const [detail, setDetail] = useState('');
 
+  //配布コード（空欄の場合にできないようにする部分のコード）
+  const isFormEmpty = !title.trim() || !detail.trim();
+  //const isInvalidMessage = Boolean(errorMessage.title || errorMessage.detail);
+  const isError = isFormEmpty // || isInvalidMessage;
+
   /**
    * TODO：新規登録の作成
    */
@@ -28,15 +33,17 @@ export const RegisterForm = ({setTaskList}:Props) => {
     setTaskList((prev)=>[...prev,newTask]);
     setTitle("");
     setDetail("");
+    
+    
   };
 
   return (
     <form style={formCard} onSubmit={(e) => onSubmitForm(e)}>
-      <input style={input} type='text' value={title} onChange={(e) => setTitle(e.target.value)} placeholder='タイトルを入力a'/>
+      <input style={input} type='text' value={title} onChange={(e) => setTitle(e.target.value)} placeholder='タイトルを入力'/>
       <br />
       <textarea style={textarea} value={detail} onChange={(e) => setDetail(e.target.value)} rows={7} placeholder='TODO を入力'></textarea>
       <div style={actions}>
-        <button style={primaryBtn(true)} type='submit'>
+        <button style={primaryBtn(true)} type='submit' disabled = {isError}>
           追加
         </button>
       </div>
